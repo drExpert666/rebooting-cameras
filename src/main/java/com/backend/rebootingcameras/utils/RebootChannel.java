fda9e34a83f0d66a38bdcb8f98bd20db5733aa9a
drexpert666 <sashkaro@gmail.com>
2022-02-06 21:10:37 +0300

    - изменена реализация перезагрузки камеры для ubuntu

--- src/main/java/com/backend/rebootingcameras/utils/RebootChannel.java
+++ src/main/java/com/backend/rebootingcameras/utils/RebootChannel.java
@@ -31,22 +31,27 @@ public class RebootChannel implements Runnable {
     @SneakyThrows
     @Override
     public void run() {
-        String setCommandOff = String.format(PathForRequest.STRING_FOR_REBOOT_CISCO,
+        /* win10 */
+//        String setCommandOff = String.format(PathForRequest.STRING_FOR_REBOOT_CISCO,
+//                rebootValues.getSwitchIp(), rebootValues.getCameraPort(), PathForRequest.STRING_FOR_REBOOT_OFF);
+//        String setCommandOn = String.format(PathForRequest.STRING_FOR_REBOOT_CISCO,
+//                rebootValues.getSwitchIp(), rebootValues.getCameraPort(), PathForRequest.STRING_FOR_REBOOT_ON);
+
+        /* ubuntu */
+        String setCommandOff = String.format(PathForRequest.STRING_FOR_REBOOT_CISCO_UBUNTU,
                 rebootValues.getSwitchIp(), rebootValues.getCameraPort(), PathForRequest.STRING_FOR_REBOOT_OFF);
-        String setCommandOn = String.format(PathForRequest.STRING_FOR_REBOOT_CISCO,
+        String setCommandOn = String.format(PathForRequest.STRING_FOR_REBOOT_CISCO_UBUNTU,
                 rebootValues.getSwitchIp(), rebootValues.getCameraPort(), PathForRequest.STRING_FOR_REBOOT_ON);
-
         System.out.println(Thread.currentThread().getName());
-        System.out.println(PathForRequest.STRING_FOR_RUN_SNMP_SET + setCommandOff);
-        System.out.println(PathForRequest.STRING_FOR_RUN_SNMP_SET + setCommandOn);
-
-        //todo изменить реализацию для сервера (убрать запуск exe файла)
+        System.out.println(setCommandOff);
+        System.out.println(setCommandOn);
         try {
             /* запускаю SnmpSet */
-            runSnmp(setCommandOff, "poe_of.bin");
+            runSnmp(setCommandOff);
+            Thread.sleep(2000);
 
             /* читаю файл */
-            openFileReader("C:/Users/romanov-av/Documents/rebooting-cameras/poe_of.bin");
+//            openFileReader("C:/Users/romanov-av/Documents/rebooting-cameras/poe_of.bin");
 
         } catch (IOException e) {
             System.out.println("Поймана во время запуска программы SnmpSet.exe " + e);
@@ -55,9 +60,9 @@ public class RebootChannel implements Runnable {
 
         try {
             /* запускаю SnmpSet */
-            runSnmp(setCommandOn, "poe_on.bin");
+            runSnmp(setCommandOn);
             /* читаю файл */
-            openFileReader("C:/Users/romanov-av/Documents/rebooting-cameras/poe_on.bin");
+//            openFileReader("C:/Users/romanov-av/Documents/rebooting-cameras/poe_on.bin");
 
         } catch (IOException e) {
             System.out.println("Поймана ошибка повторного открытия файла " + e);
@@ -66,6 +71,7 @@ public class RebootChannel implements Runnable {
 
     }
 
+    //todo убрать лишний метод
     private void openFileReader(String filePath) throws IOException {
         BufferedReader reader =
                 new BufferedReader(new FileReader(filePath));
@@ -81,13 +87,36 @@ public class RebootChannel implements Runnable {
         wasErrorOnChangePoeStatus = !result.contains("OK");
     }
 
-    private void runSnmp(String setCommand, String fileName) throws IOException, InterruptedException {
-        Process process = Runtime.getRuntime().exec(PathForRequest.STRING_FOR_RUN_SNMP_SET + setCommand);
-        DataOutputStream outputStream = new DataOutputStream(
-                new FileOutputStream(fileName)); // создаю поток данных для записи в этот файл
-        process.getInputStream().transferTo(outputStream);
-        process.destroy();
-        wasErrorOnExecuteSnmp = false;
-        Thread.sleep(4000);
+    private void runSnmp(String setCommand) throws IOException{
+        Process process = Runtime.getRuntime().exec(
+//                PathForRequest.STRING_FOR_RUN_SNMP_SET + /* win10 */
+                setCommand);
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+        System.out.println(builder.toString());
+        wasErrorOnChangePoeStatus = !builder.toString().contains("INTEGER");
+        try {
+            process.waitFor();
+            wasErrorOnExecuteSnmp = false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            wasErrorOnExecuteSnmp = true;
+        }
+
+
+        /* win10 */
+//        DataOutputStream outputStream = new DataOutputStream(
+//                new FileOutputStream(fileName)); // создаю поток данных для записи в этот файл
+//        process.getInputStream().transferTo(outputStream);
+//        process.destroy();
+//        wasErrorOnExecuteSnmp = false;
+//        Thread.sleep(4000);
     }
 }
